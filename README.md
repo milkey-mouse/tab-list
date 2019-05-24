86cba5f2cd751cab4367d364645c11ca8398fb9f
Milkey Mouse <milkeymouse@meme.institute>
2019-05-24 12:59:33 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tab List
+
+Tab List is a Chrome extension to list URLs of all open tabs in a copy-pastable format.
+
+# Credits
+
+The Tab List icon was created by [Icons8](https://icons8.com).
---
+++ tab-list/popup.js
@@ -0,0 +1,30 @@
+chrome.tabs.query({},function(tabs) {
+  let ul = document.createElement("ul");
+  tabs.forEach(function(tab) {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    a.setAttribute("target", "_blank");
+    a.setAttribute("href", tab.url);
+    let img = document.createElement("img");
+    img.setAttribute("src", tab.favIconUrl || "placeholder.png");
+    a.appendChild(img);
+    let span = document.createElement("span");
+    span.textContent = tab.title;
+    a.appendChild(span);
+    li.appendChild(a);
+    ul.appendChild(li);
+  });
+  document.body.appendChild(ul);
+
+  let btn = document.createElement("button");
+  btn.setAttribute("type", "button");
+  btn.textContent = "Copy";
+  btn.addEventListener("click", function() {
+    navigator.clipboard.writeText(
+      tabs.map(function (tab) {
+        return tab.title ? (tab.url + " (" + tab.title + ")") : tab.url;
+      }).join("\n")
+    );
+  });
+  document.body.appendChild(btn);
+});
