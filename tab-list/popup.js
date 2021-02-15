41a0c1b180f508d697a671c76c49d21d5554c75c
Milkey Mouse <milkeymouse@meme.institute>
2021-02-14 19:51:56 -0800

    Move copy button to top of popup

--- tab-list/popup.js
+++ tab-list/popup.js
@@ -1,4 +1,16 @@
 chrome.tabs.query({},function(tabs) {
+  let btn = document.createElement("button");
+  btn.setAttribute("type", "button");
+  btn.textContent = "Copy";
+  btn.addEventListener("click", function() {
+    navigator.clipboard.writeText(
+      tabs.map(function (tab) {
+        return tab.title ? (tab.url + " (" + tab.title + ")") : tab.url;
+      }).join("\n")
+    );
+  });
+  document.body.appendChild(btn);
+
   let ul = document.createElement("ul");
   tabs.forEach(function(tab) {
     let li = document.createElement("li");
@@ -15,16 +27,4 @@ chrome.tabs.query({},function(tabs) {
     ul.appendChild(li);
   });
   document.body.appendChild(ul);
-
-  let btn = document.createElement("button");
-  btn.setAttribute("type", "button");
-  btn.textContent = "Copy";
-  btn.addEventListener("click", function() {
-    navigator.clipboard.writeText(
-      tabs.map(function (tab) {
-        return tab.title ? (tab.url + " (" + tab.title + ")") : tab.url;
-      }).join("\n")
-    );
-  });
-  document.body.appendChild(btn);
 });
